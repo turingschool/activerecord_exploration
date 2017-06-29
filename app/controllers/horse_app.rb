1f9bef6a74d13864213af622b634ca4e4ea1a2ac
Katie Shermer <katieshermer@gmail.com>
2017-06-29 08:42:04 -0600

    Corrected jockey/:id to jockeys/:id

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -1,11 +1,17 @@
 class HorseApp < Sinatra::Base
+
   get '/horses' do
     @horses = Horse.all
     erb :"horses/index"
   end
 
-  get '/jockey/:id' do
+  get '/horses/:id' do
+
+  end
+
+  get '/jockeys/:id' do
     @jockey = Jockey.find(params[:id])
     erb :"jockeys/jockey"
   end
+
 end
