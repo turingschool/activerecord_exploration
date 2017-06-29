ce9c08ad13a7736f283bc6c606eedac078b344e9
JoelLindow <joellindow@gmail.com>
2017-06-28 20:46:11 -0600

    tables built and added relationships

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -1,6 +1,13 @@
 class HorseApp < Sinatra::Base
+
   get '/horses' do
     @horses = Horse.all
     erb :"horses/index"
   end
+
+  get '/jockeys/:id' do
+    @jockey = Jockey.find(:id)
+    erb :"jockeys/show"
+  end
+
 end
