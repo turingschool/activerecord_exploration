eb1c1c650cb3f0eb0e14af209ccb63ad24fcb3e5
JoelLindow <joellindow@gmail.com>
2017-06-28 21:44:01 -0600

    clean up model

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,13 +1,4 @@
 class Horse < ActiveRecord::Base
   belongs_to :jockey
   validates :name, presence: true
-
-  def self.average_winnings
-    average(:total_winnings).round
-  end
-
-  def self.total_winnings
-    sum(:total_winnings)
-  end
-
 end
