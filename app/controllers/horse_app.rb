062d5f48ff50d521ee6aceffa383641fcd731924
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-28 22:18:38 -0600

    last commit for the night, can't get erb: new to work

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -1,6 +1,28 @@
 class HorseApp < Sinatra::Base
+  set :method_override, true
+
   get '/horses' do
     @horses = Horse.all
     erb :"horses/index"
   end
+
+  get '/horses/:id' do
+    @horse = Horse.find_by_id(params[:id])
+    erb :"horses/horse"
+  end
+
+  get '/horses/new' do
+    erb :"horses/new"
+  end
+
+  get '/horses/:id/edit' do
+    @horse = Horse.find_by_id(params[:id])
+    erb :"horses/edit"
+  end
+
+  put '/horses/:id' do
+    Horse.update(params[:id].to_i, params[:horse])
+    redirect "/horses"
+  end
+
 end
